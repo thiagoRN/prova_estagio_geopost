add641e162467088d7f57b3f2e39635313aac2da
thiagoRN <thiago_rn@hotmail.com>
2022-10-13 22:20:59 -0300

    teste tabalea de grupos table e css

--- script.js
+++ script.js
@@ -28,33 +28,7 @@ const loadAll = async () => {
 }
 
 
-// const grupos = async () => {
-//     let tea = await loadAll();
-//     let newarr=[];
-//     for (var i = 0; i < tea.length; i = i + 4) {
-//         newarr.push(tea.slice(i, i + 4));
-//       }
-    
-//     return newarr;
-// }
-
-// grupos()
-
-
-// const main = async () =>{
-//     let team = await loadAll();
-//     console.log(team);
-    
-
-//     let teams = document.getElementById('grupoA');
-//     team.map((element,index) => {
-//         if(index <3){ var linha = grupesList(element.Name);
-//             teams.appendChild(linha)}
-         
-//     });
-// }
-
-const main = async () =>{
+const grupes = async () =>{
     let team = await loadAll();
     console.log(team);
     
@@ -67,31 +41,35 @@ const main = async () =>{
     let grupoF = document.getElementById('grupoF');
     let grupoG = document.getElementById('grupoG');
     let grupoH = document.getElementById('grupoH');
+    let td = document.getElementById('td-p');
     team.map((element,index) => {
         if(index <= 3){
-             var linha = grupesList(element.Name);
-            grupoA.appendChild(linha)
+             var g = grupesList(element.Name);
+             var t = grupesList(element.Name);
+
+            td.appendChild(t); 
+            grupoA.appendChild(g)
         }if (index >= 4 && index <= 7){
-            var linha = grupesList(element.Name);
-            grupoB.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoB.appendChild(g)
         }if (index >= 8 && index <= 11){
-            var linha = grupesList(element.Name);
-            grupoC.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoC.appendChild(g)
         }if (index >= 12 && index <= 15){
-            var linha = grupesList(element.Name);
-            grupoD.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoD.appendChild(g)
         }if (index >= 16 && index <= 19){
-            var linha = grupesList(element.Name);
-            grupoE.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoE.appendChild(g)
         }if (index >= 20 && index <= 23){
-            var linha = grupesList(element.Name);
-            grupoF.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoF.appendChild(g)
         }if (index >= 24 && index <= 27){
-            var linha = grupesList(element.Name);
-            grupoG.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoG.appendChild(g)
         }if (index >=28){
-            var linha = grupesList(element.Name);
-            grupoH.appendChild(linha)
+            var g = grupesList(element.Name);
+            grupoH.appendChild(g)
         }
     });
 }
@@ -99,38 +77,189 @@ const main = async () =>{
 
 
 const grupesList = (element) =>{
-    let name = document.createElement("div");
-    name.setAttribute("id", `grupe-div`);
+    let name = document.createElement("a");
+    name.setAttribute("id", `grupe-a`);
     name.innerHTML = element;
     
     return name
 }
 
-// const grupesList = (element,index) =>{
-
-//     if(index < 3){
-//         let name = document.getElementById('grupoA');
-//         name.appendChild(element)
-//     }
-//     let name = document.createElement("div");
-//     name.setAttribute("id", `grupe-div`);
-//     name.innerHTML = element;
+const grupesLis = (element) =>{
+    let name = document.createElement("p");
+    name.setAttribute("id", `td-score`);
+    name.innerHTML = element;
     
-//     return name
-// }
+    return name
+}
 
-// const teamsList = async (item) =>{
-//     let grupe =  await item;
 
-//     let grupes = document.getElementById('grupe-div')
-//     grupe.map((element, index) => {
+const resultados = () => {
+    timeA = {
+        J: 0,
+        V: 0,
+        E: 0,
+        D: 0,
+        G: 0,
+        P:0
+    }
+    timeB = {
+        J: 0,
+        V: 0,
+        E: 0,
+        D: 0,
+        G: 0,
+        P:0
+    }
+    timeC = {
+        J: 0,
+        V: 0,
+        E: 0,
+        D: 0,
+        G: 0,
+        P:0
+    }
+    timeD = {
+        J: 0,
+        V: 0,
+        E: 0,
+        D: 0,
+        G: 0,
+        P:0
+    }
+
+   for(let i = 0; i < 3; i++){
+    let A = Math.floor(Math.random() * 6);
+    let B = Math.floor(Math.random() * 6);
+    let C = Math.floor(Math.random() * 6);
+    let D = Math.floor(Math.random() * 6);
+
+    switch (i) {
+        case 0:
+            if(A < B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.D += 1;timeB.V += 1;
+                timeA.G += A;timeB.G += B;
+                timeB.P += 3;
+            }else if(A > B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.V += 1;timeB.D += 1;
+                timeA.G += A;timeB.G += B;
+                timeA.P += 3;
+            }else if(A = B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.E += 1;timeB.E += 1;
+                timeA.G += A;timeB.G += B;
+                timeA.P += 1;timeB.P += 1;
+            }
+
+            if(C < D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.D += 1;timeD.V += 1;
+                timeC.G += C;timeD.G += D;
+                timeD.P += 3;
+            }else if(C > D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.V += 1;timeD.D += 1;
+                timeC.G += C;timeD.G += D;
+                timeC.P += 3;
+            }else if(C = D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.E += 1;timeD.E += 1;
+                timeC.G += C;timeD.G += D;
+                timeC.P += 1;timeD.P += 1;
+            }
+
+
+        break;
+
+        case 1:
+            if(A < B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.D += 1;timeB.V += 1;
+                timeA.G += A;timeB.G += B;
+                timeB.P += 3;
+            }else if(A > B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.V += 1;timeB.D += 1;
+                timeA.G += A;timeB.G += B;
+                timeA.P += 3;
+            }else if(A = B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.E += 1;timeB.E += 1;
+                timeA.G += A;timeB.G += B;
+                timeA.P += 1;timeB.P += 1;
+            }
+
+            if(C < D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.D += 1;timeD.V += 1;
+                timeC.G += C;timeD.G += D;
+                timeD.P += 3;
+            }else if(C > D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.V += 1;timeD.D += 1;
+                timeC.G += C;timeD.G += D;
+                timeC.P += 3;
+            }else if(C = D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.E += 1;timeD.E += 1;
+                timeC.G += C;timeD.G += D;
+                timeC.P += 1;timeD.P += 1;
+            }
+            
         
-//         let name = document.createElement("a");
-//         name.innerHTML = element.Name;
-//         grupes.appendChild(name)
-//    });
+        break;
+        
+        case 2:
+            if(A < B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.D += 1;timeB.V += 1;
+                timeA.G += A;timeB.G += B;
+                timeB.P += 3;
+            }else if(A > B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.V += 1;timeB.D += 1;
+                timeA.G += A;timeB.G += B;
+                timeA.P += 3;
+            }else if(A = B){
+                timeA.J += 1;timeB.J += 1;
+                timeA.E += 1;timeB.E += 1;
+                timeA.G += A;timeB.G += B;
+                timeA.P += 1;timeB.P += 1;
+            }
+
+            if(C < D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.D += 1;timeD.V += 1;
+                timeC.G += C;timeD.G += D;
+                timeD.P += 3;
+            }else if(C > D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.V += 1;timeD.D += 1;
+                timeC.G += C;timeD.G += D;
+                timeC.P += 3;
+            }else if(C = D){
+                timeC.J += 1;timeD.J += 1;
+                timeC.E += 1;timeD.E += 1;
+                timeC.G += C;timeD.G += D;
+                timeC.P += 1;timeD.P += 1;
+            }
+            
+        
+        break;
+        
+        default:
+                
+    }
     
-// }
+    
+   } 
+   console.log(timeA)
+   console.log(timeB)
+   console.log(timeC)
+   console.log(timeD)
+}
 
+resultados();
 
-main();
+grupes();
